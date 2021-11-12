07f0988380ac85f066d0ffff696eaf7ae345bdd7
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-11 22:59:23 -0500

    Refactored for loop from main.go => deck.go

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 // CREATE A NEW TYPE OF 'DECK'
 // WHICH IS A SLICE OF STRINGS
 type deck []string
+
+func(d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
